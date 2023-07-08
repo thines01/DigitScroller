474ec29b55538cca6ac9e7e03b0e7f37aa6b8c2d
thines01 <thines01@yahoo.com>
2023-07-07 19:07:20 -0500

    Add project files.

---
+++ DigitScroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+namespace DigitScroller
+{
+   public class CDigitScroller
+   {
+      private ulong _ulngDigits { get; set; }
+      private char _chrPadding { get; set; }
+
+      public CDigitScroller()
+      {
+         _ulngDigits = 0;
+         _chrPadding = '0';
+      }
+
+      public CDigitScroller(ulong ulngDigits)
+      {
+         _ulngDigits = ulngDigits;
+         _chrPadding = '0';
+      }
+
+      public CDigitScroller(ulong ulngDigits, char chrPadding)
+      {
+         _ulngDigits = ulngDigits;
+         _chrPadding = chrPadding;
+      }
+
+      public string Scroll(ushort numPlaces)
+      {
+         return string.Format("{0}{1}",
+            (_ulngDigits++).ToString().PadLeft(numPlaces, _chrPadding),
+            new string(Enumerable.Repeat<char>('\b', numPlaces).ToArray()));
+      }
+
+      public string Scroll()
+      {
+         ushort numPlaces = (ushort)_ulngDigits.ToString().Length;
+
+         return string.Format("{0}{1}",
+            (_ulngDigits++).ToString().PadLeft(numPlaces, _chrPadding),
+            new string(Enumerable.Repeat<char>('\b', numPlaces).ToArray()));
+      }
+   }
+}
